c393a365665c29e94e344d50d7c94dd06cf132b0
Joblad <10959420@uvu.edu>
2021-02-06 16:38:09 -0700

    Driving Assignment

---
+++ Driving Assignment/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float turnspeed = 25.0f;
+    private float speed = 12.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //This is where we get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        //Move the Vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime *speed * forwardInput);
+        //We turn the Vehicle
+        transform.Rotate(Vector3.up, Time.deltaTime * turnspeed * horizontalInput);
+    }
+}
